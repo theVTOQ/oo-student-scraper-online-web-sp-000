c2428d5053e2921ba266224e3166939c7ab7717f
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-12-01 17:43:17 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,24 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    students = {}
+    html = File.read(index_url)
+    index_page = Nokogiri::HTML(html)
+    index_page.css("div.student-card a div.card-text-container").each do |student_card|
+      name = student_card.css("h4").text
+      location = student_card.css("p").text
+      students << {name: name, location: location}
+    end
+
+    students
   end
 
   def self.scrape_profile_page(profile_url)
+    scraped_student = {}
+    html = File.read(index_url)
+    profile_page = Nokogiri::HTML(html)
     
+    scraped_student
   end
 
 end
-
