ad6affcd67fe1e3de113857ca83d4be392f397dd
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-12-01 20:28:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,25 +22,23 @@ class Scraper
     html = File.read(profile_url)
     profile_page = Nokogiri::HTML(html)
     social_media = profile_page.css("div.social-icon-container a")
-    twitter = social_media[0]['href']
-    linkedin = social_media[1]['href']
-    github = social_media[2]['href']
-    blog = social_media[3]['href']
+    possible_sites = ["linkedin", "github", "twitter"]
+    social_media.each do |site|
+      site_link = site['href']
+      site_name = site_link[/#{"www."}(.*?)#{".com"}/m, 1]
+      if site_name == nil
+        site_name = site_link[/#{"\/\/"}(.*?)#{".com"}/m, 1]
+      elsif !possible_sites.include?(site_name)
+        site_name =  "blog"
+      end
+      scraped_student[site_name.to_sym] = site_link
+    end
 
     profile_quote = profile_page.css("div.vitals-text-container div.profile-quote").text
     bio = profile_page.css("div.details-container p").text
 
-    scraped_student[:twitter] = twitter unless twitter == nil
-    scraped_student[:linkedin] = linkedin unless twitter == nil
-    scraped_student[:github] = github unless twitter == nil
-    scraped_student[:blog] = blog
     scraped_student[:profile_quote] = profile_quote
     scraped_student[:bio] = bio
     scraped_student
   end
 end
-
-html = File.read("./fixtures/student-site/students/joe-burgess.html")
-profile_page = Nokogiri::HTML(html)
-social_media = profile_page.css("div.social-icon-container a")
-puts social_media[0]
